fc68c011563a68972f7b6ef09a6037cec7b4fdad
Pascal Bertschi <pascal@bertschi.io>
2020-07-09 21:19:50 +0200

    added tailwind ui components

---
+++ src/AboutUs.svelte
@@ -0,0 +1,8 @@
+<div class="bg-gray-800">
+
+    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <p class="text-center text-base leading-6 font-semibold uppercase text-white tracking-wider">
+            Unsere Team
+        </p>
+    </div>
+</div>
